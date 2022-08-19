03823042fe70043672f132e073e6a7cab77211b9
rodrigoperochena <rodrigoperochena@gmail.com>
2022-08-19 22:20:36 +0800

    Update _main.js

--- src/_includes/_main.js
+++ src/_includes/_main.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const navTab = document.querySelector('.nav-tab')
+  const navTabTitleMenu = document.querySelector('.nav-tab-title--menu')
+  const navTabTitleClose = document.querySelector('.nav-tab-title--close')
   const burgerIcon = document.querySelector('.burger')
   const navMenu = document.querySelector('.nav-wrapper')
 
-  burgerIcon.addEventListener('click', () => {
+  navTab.addEventListener('click', () => {
     navMenu.classList.toggle('menu-open')
+
+    if (navMenu.classList.contains('menu-open')) {
+      navTabTitleClose.style.display = 'block'
+      navTabTitleMenu.style.display = 'none'
+    } else {
+      navTabTitleClose.style.display = 'none'
+      navTabTitleMenu.style.display = 'block'
+    }
+
+    if (burgerIcon.classList.contains('x')) {
+      burgerIcon.classList.remove('x')
+    } else {
+      burgerIcon.classList.add('x')
+    }
   })
 })
