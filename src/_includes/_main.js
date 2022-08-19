9fbeb7837d58c79d3103e4eb260d3a6ef4b6b9f8
rodrigoperochena <rodrigoperochena@gmail.com>
2022-08-19 22:24:29 +0800

    Update _main.js

--- src/_includes/_main.js
+++ src/_includes/_main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navTab = document.querySelector('.nav-tab')
+  const navMenu = document.querySelector('.nav-wrapper')
   const navTabTitleMenu = document.querySelector('.nav-tab-title--menu')
   const navTabTitleClose = document.querySelector('.nav-tab-title--close')
   const burgerIcon = document.querySelector('.burger')
-  const navMenu = document.querySelector('.nav-wrapper')
 
   navTab.addEventListener('click', () => {
     navMenu.classList.toggle('menu-open')
