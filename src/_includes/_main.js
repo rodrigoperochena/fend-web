2f3c6082688d142b2ec82078f618f3b0bcef9b07
rodrigoperochena <rodrigoperochena@gmail.com>
2022-08-22 14:08:50 +0800

    Update _main.js

--- src/_includes/_main.js
+++ src/_includes/_main.js
@@ -4,16 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const navTabTitleMenu = document.querySelector('.nav-tab-title--menu')
   const navTabTitleClose = document.querySelector('.nav-tab-title--close')
   const burgerIcon = document.querySelector('.burger')
+  const pageOverlay = document.querySelector('.page-overlay')
+  const pageOverlayActive = document.querySelector('.page-overlay--active')
 
   navTab.addEventListener('click', () => {
     navMenu.classList.toggle('menu-open')
 
     if (navMenu.classList.contains('menu-open')) {
-      navTabTitleClose.style.display = 'block'
+      navTabTitleClose.style.display = 'flex'
       navTabTitleMenu.style.display = 'none'
+      pageOverlay.classList.add('page-overlay--active')
     } else {
       navTabTitleClose.style.display = 'none'
-      navTabTitleMenu.style.display = 'block'
+      navTabTitleMenu.style.display = 'flex'
+      pageOverlay.classList.remove('page-overlay--active')
     }
 
     if (burgerIcon.classList.contains('x')) {
@@ -22,4 +26,21 @@ document.addEventListener('DOMContentLoaded', () => {
       burgerIcon.classList.add('x')
     }
   })
+
+  pageOverlay.addEventListener('click', () => {
+    navMenu.classList.toggle('menu-open')
+    burgerIcon.classList.remove('x')
+    pageOverlay.classList.remove('page-overlay--active')
+    navTabTitleClose.style.display = 'none'
+    navTabTitleMenu.style.display = 'flex'
+  })
+
+  document.body.addEventListener('keyup', (a) => {
+    if (a.key === 'Escape' && pageOverlay.classList.contains('page-overlay--active')) {
+      navMenu.classList.remove('menu-open')
+      burgerIcon.classList.remove('x')
+      pageOverlay.classList.remove('page-overlay--active')
+    }
+  })
+
 })
