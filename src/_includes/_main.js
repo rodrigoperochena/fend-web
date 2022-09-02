59726665df72c50a862bb8d5855bbf0eac1ebfcb
rodrigoperochena <rodrigoperochena@gmail.com>
2022-09-02 19:10:32 +0800

    Update _main.js

--- src/_includes/_main.js
+++ src/_includes/_main.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navTab = document.querySelector('.nav-tab')
-  const navMenu = document.querySelector('.nav-wrapper')
+  const navMenu = document.querySelector('nav')
   const navTabTitleMenu = document.querySelector('.nav-tab-title--menu')
   const navTabTitleClose = document.querySelector('.nav-tab-title--close')
   const burgerIcon = document.querySelector('.burger')
   const pageOverlay = document.querySelector('.page-overlay')
-  const pageOverlayActive = document.querySelector('.page-overlay--active')
 
   navTab.addEventListener('click', () => {
-    navMenu.classList.toggle('menu-open')
+    navMenu.classList.toggle('nav--open')
 
-    if (navMenu.classList.contains('menu-open')) {
+    if (navMenu.classList.contains('nav--open')) {
       navTabTitleClose.style.display = 'flex'
       navTabTitleMenu.style.display = 'none'
       pageOverlay.classList.add('page-overlay--active')
@@ -28,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   pageOverlay.addEventListener('click', () => {
-    navMenu.classList.toggle('menu-open')
+    navMenu.classList.toggle('nav--open')
     burgerIcon.classList.remove('x')
     pageOverlay.classList.remove('page-overlay--active')
     navTabTitleClose.style.display = 'none'
@@ -37,12 +36,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.body.addEventListener('keyup', (a) => {
     if (a.key === 'Escape' && pageOverlay.classList.contains('page-overlay--active')) {
-      navMenu.classList.remove('menu-open')
+      navMenu.classList.remove('nav--open')
       burgerIcon.classList.remove('x')
       pageOverlay.classList.remove('page-overlay--active')
       navTabTitleClose.style.display = 'none'
       navTabTitleMenu.style.display = 'flex'
     }
   })
+
+
+  // BLOCKPICKING MOCKUP - UPDATING ELEMENTS
+  const gridButton = document.querySelector('.grid-button')
+  const centerBlock = document.querySelector('.center-block')
+
+  gridButton.addEventListener('click', () => {
+    centerBlock.classList.toggle('block-active')
+    updateElements()
+  })
+
+  let updateElements = () => {
+    const buyQuantity = document.querySelector('.number')
+    const price = document.querySelector('.header-button-pricetag')
+
+    if (centerBlock.classList.contains('block-active')) {
+      buyQuantity.innerText = '0.5 '
+      price.innerText = '$60'
+    } 
+    
+    else {
+      buyQuantity.innerText = '0.0 '
+      price.innerText = '$0'
+    }
+
+  }
 
 })
